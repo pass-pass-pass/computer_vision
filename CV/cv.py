0bbfd2974a51af0bb3e2865ea0a851bdbc616588
pass-pass-pass <forgithubuse@gmail.com>
2023-06-12 21:01:22 -0500

    m

--- CV/cv.py
+++ CV/cv.py
@@ -1,19 +1,17 @@
 import cv2 as cv
 img = cv.imread('cat.img')
 
-cv.waitKey(0)
+cv.waitKey(0, img)
+
+
+#  if there are too large images it goes off screen
 
 
 
+#  rading videos
 
-
-
-
+capt = cv.VideoCapture('/dog.mp4')
 
 
 
 print('hello world')
-
-
-
-
