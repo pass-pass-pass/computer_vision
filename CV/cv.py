965b94e8d3669301ccf8b9bcd48e8d83c924709e
pass-pass-pass <forgithubuse@gmail.com>
2023-06-20 17:30:48 -0500

    m

--- CV/cv.py
+++ CV/cv.py
@@ -61,14 +61,14 @@ print(tensor1.ndim)
 print(tensor1.shape)
 random = torch.rand(3,4)
 
+zeros = torch.arange(start = 0 , end = 32, step = 1)
 
-zeros = torch.rand()
 one_to = torch.arange(start = 1 ,end  = 1089, step = 123 )
 
 torch.matmul(one_to, one_to)
-# find mean
-torch.min(zeros)
-zeros.min()
+# # find mean
+torch.min(zeros, dim = 0)
+zeros.min( zeros)
 torch.max(zeros)
 torch.sum(zeros)
 
@@ -78,10 +78,12 @@ zeros.argmin()
 # max
 zeros.argmax()
 
+
+s = np.zeros((3,2))
 #  from numpy to pytorch
-tensor3 = torch.from_numpy()
+tensor3 = torch.from_numpy(s)
 #  from pytorch to numpy
-tensor4 = TENSOR.numpy()
+tensor4 = zeros.numpy()
 
 
 
@@ -90,3 +92,19 @@ tensor4 = TENSOR.numpy()
 
 random_tensor = torch.rand(3,4)
 random_tensor_b = torch.rand(3,4)
+
+#  set the seed for the random   the same seed produce the same random number
+torch.manual_seed(32)
+
+#  reproduciblity
+print(torch.cuda.is_available())
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# device
+tensor = torch.tensor([1,2,3])
+print(tensor.device)
+#  movev tensor to gpu
+tensor_gpu = tensor.to(device)
+print(tensor_gpu , tensor_gpu.device)
+
+
