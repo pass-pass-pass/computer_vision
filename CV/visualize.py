bd20414a2e94d1483eb54940c494bb8d44a72111
pass-pass-pass <forgithubuse@gmail.com>
2023-07-02 19:59:33 -0500

    m

--- CV/visualize.py
+++ CV/visualize.py
@@ -10,11 +10,17 @@ x= df.values[2:,0]
 time = df.values[0, :]
 y = df.values[2:, 1]
 z = df.values[2:, 2]
-plt.figure(figsize=(8,8))
-plt.subplot(3,1,1)
-plt.plot(x, y)  
-plt.show()
+# plt.figure(figsize=(8,8))
+# plt.subplot(3,1,1)
+# plt.plot(x, y)  
+# plt.show()
 
+df.sample(19)
 
-
+df.loc[12:14]
+# new line to test thoughts
+from urllib.request import urlretrieve
+urlretrieve('https://hub.jovian.ml/wp-content/uploads/2020/08/climate.csv')
+covid_Df = pd.read_csv('taly-covid-daywise.csv')
+print(covid_Df)
 
